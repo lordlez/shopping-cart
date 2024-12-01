f01f5a318083da0d4a31dd4cb887292ccf791361
Lorenzo Lezcano <lorenzodlezcano@gmail.com>
2024-11-30 21:55:26 -0300

    feat: base

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { db } from "./data/db";
+
+function App() {
+  const [data, setData] = useState(db);
+
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar key={guitar.id} guitar={guitar} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
